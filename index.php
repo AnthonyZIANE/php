33dbf55a3e97476356d7d0a0701f3c5c6a044f78
z18011806 <anthony.ziane@etu.univ-amu.fr>
2019-09-13 10:57:54 +0200

    variables test2 + br

--- index.php
+++ index.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <title>Title</title>
 </head>
-<body>
+<br>
 <?php
 $var1 =6;
 $var2 = 1.3;
 $var3 = 'Variable 3';
 ?>
 <?php echo "$var1 + $var2"; ?>
+<br><?php echo $var1 + $var2; ?></br>
 
 <?php
 echo '<br><strong> Voici mon premier programme PHP </strong><br/>' . "\n";
