0b7b184e5b46bd1446bacfe23bc43e76477a70b3
z18011806 <anthony.ziane@etu.univ-amu.fr>
2019-09-13 11:06:11 +0200

    Mutualisation de code

--- index.php
+++ index.php
@@ -1,22 +1,21 @@
+<?php
+function start_page($title)
+{
+    echo ' <!DOCTYPE html> <html
+lang="fr"><head><title>' . PHP_EOL . $title . '</title></head><body>' . PHP_EOL
+    ;
+};
+?>
+<?php
+start_page('YASS');
+?>
 <!DOCTYPE html>
 <br lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
 </head>
-<br>
-<?php
-$var1 =6;
-$var2 = 1.3;
-$var3 = 'Variable 3';
-?>
-<?php echo "$var1 + $var2"; ?>
-<br><?php echo $var1 + $var2; ?></br>
-<br><?php echo $var1 + $var3; ?></br>
+<body>
 
-<?php
-echo '<br><strong> Voici mon premier programme PHP </strong><br/>' . "\n";
-echo 'C\'est pas mal non ? <br/>';
-?>
 </body>
 </html>
