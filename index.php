7f515b006ff8dd1b3d56263fb1d481c9542ec5ba
z18011806 <anthony.ziane@etu.univ-amu.fr>
2019-09-13 11:16:57 +0200

    Date 1.4

--- index.php
+++ index.php
@@ -9,11 +9,13 @@ lang="fr"><head><title>' . PHP_EOL . $title . '</title></head><body>' . PHP_EOL
 <?php
 start_page('YASS');
 ?>
+<?php
+echo date('l F d, Y');
+?>
 
 <?php
 $jour = date('l F d, Y');
 ?>
-
 
 <!DOCTYPE html>
 <br lang="en">
