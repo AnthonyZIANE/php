74ff958fcc74a57f81db860db48a7e32faf2415f
Ziane Anthony <ant>
2019-09-19 20:07:15 +0200

    1.1 mdp

--- test-pass.php
+++ test-pass.php
@@ -8,14 +8,14 @@ $mdp = $_POST['Mot De Passe'];
 
 if($log == 'login' && $mdp =='mdp')
 {
-    header('Location: page2.php');
+    header('Location: accueil.php');
 }
 
 else
 
 {
 
-    echo '<br/><strong>Bouton non géré !</strong><br/>';
+    header('Location: fail.php');;
 
 }
 
