faa13af31be00974cc700d33d8a26015938d0955
Ziane Anthony <ant>
2019-09-19 20:50:31 +0200

    1.1 mdp

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 
-
+echo $_GET['step'];
 
 
 
--- test-pass.php
+++ test-pass.php
@@ -1,21 +1,38 @@
 <?php
 
+
+
+$dbLink = mysqli_connect( 'mysql-anthonyziane.alwaysdata.net','189623', 'Voiture123')
+or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
+
+mysqli_select_db($dbLink , 'anthonyziane_tp2')
+or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink)
+);
+
+
+
 $log = $_POST['login'];
 $mdp = $_POST['mdp'];
 
 
 
+$query = 'SELECT * FROM user WHERE login = \'' . $log . '\'and \'' . $mdp . '\'';
 
-if($log == 'joe' && $mdp =='yes')
+if (!($dbResult = mysqli_query($dbLink, $query)))
 {
-    header('Location: oui.php');
+    echo 'Erreur de requête <br/>';
+    //Affiche le type d'erreur.
+    echo'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+    //Affiche la requête envoyée.
+    echo'Requête : ' . $query . '<br/>';
+    exit();
 }
 
 else
 
 {
 
-    header('Location: fail.php');
+    header('Location: login.php');
 
 }
 
