8b5f53c294df9b7e5672db47ad8c9377f095d54f
z18011806 <anthony.ziane@etu.univ-amu.fr>
2019-09-25 16:02:09 +0200

    TD3 2.1

---
+++ Menu.php
@@ -0,0 +1,30 @@
+<?php
+
+$aa = 'Page 1';
+$aaa= 'calcul.php';
+
+?>
+
+
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Titre de la page</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+</head>
+<body>
+
+<ul id="menu">
+
+    <li><a href="<?php echo $aaa;?>"><?php echo $aa;?></a></li>
+
+
+</ul>
+
+
+
+</body>
+</html>
