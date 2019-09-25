4e27ab1ad264f39315911c5408d26e6c52c30cb1
z18011806 <anthony.ziane@etu.univ-amu.fr>
2019-09-25 14:44:08 +0200

    6.4

--- test-pass.php
+++ test-pass.php
@@ -12,13 +12,16 @@ $mdp = $_POST['mdp'];
 if($log == 'Joe' && $mdp =='voiture')
 {
     $_SESSION['login'] = 'ok';
+    $_SESSION['mdp'] = 'ok';
     header('Location: oui.php');
 }
+
+
 
 else
 
 {
-
+    die ('Erreur');
     header('Location: login.php?step=ERROR');
 
 }
